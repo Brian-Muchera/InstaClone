335f3e0b927a7a697bf28a8512ecedd63c6bbea3
Brian-Muchera <mucherabrian2@gmail.com>
2020-10-19 23:30:45 +0300

    added db

--- Snapgram/settings.py
+++ Snapgram/settings.py
@@ -116,6 +116,19 @@ USE_L10N = True
 USE_TZ = True
 
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'instagram',
+        'USER': 'muchera-brian',
+    'PASSWORD':'insta',
+    }
+}
+
+
+
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
